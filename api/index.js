e40c0fa78dc49ad51e7c40872a45964a0f870f39
Julio Cesar <jccintr@gmail.com>
2024-01-26 00:35:06 -0300

    criado back end

---
+++ api/index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(3000,()=>{
+   console.log('run on 3000 port!');
+});
--- blog-frontend/src/App.jsx
+++ blog-frontend/src/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Dashboard from './pages/Dashboard';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+         <Route path="/" element={<Home/>}/>
+         <Route path="/about" element={<About/>}/>
+         <Route path="/projects" element={<Projects/>}/>
+         <Route path="/dashboard" element={<Dashboard/>}/>
+         <Route path="/signin" element={<SignIn/>}/>
+         <Route path="/signup" element={<SignUp/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
